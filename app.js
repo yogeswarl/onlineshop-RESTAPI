326f7bebc0a85c26597466623d7a8e7c522ddc78
yogeswar <yogi2198@gmail.com>
2020-05-10 19:52:27 +0530

    adding pagination and able to create posts

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const graphqlHttp= require('express-graphql')
 const graphqlSchema = require('./graphql/schema')
 const graphqlResolvers = require('./graphql/resolvers')
 
+const auth = require('./middleware/auth');
+
 const app = express();
 
 const fileStorage = multer.diskStorage({
@@ -50,6 +52,8 @@ app.use((req, res, next) => {
 	}
   next();
 });
+
+app.use(auth);
 
 app.use('/graphql',graphqlHttp({
   schema: graphqlSchema,
